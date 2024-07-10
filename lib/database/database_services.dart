1d1cf9275fdd526dd01bf232490017caa8a53974
supriya gupta <supriyag75@gmail.com>
2024-07-10 12:29:47 +0530

    Create User Table

--- lib/database/database_services.dart
+++ lib/database/database_services.dart
@@ -5,6 +5,11 @@ import 'package:path/path.dart';
 class DatabaseServices{
   static final DatabaseServices instance = DatabaseServices._constructor();
 
+  final String _UserTableName = "User";
+  final String _UserID = "ID";
+  final String _UserName = "Name";
+  final String _UserHeight = "Height";
+
   DatabaseServices._constructor();
 
   Future<Database> getDatabase() async{
@@ -12,6 +17,9 @@ class DatabaseServices{
     final databasePath = join(databaseDirPath, "masterDB.db");
     final database = await openDatabase(
         databasePath,
+      onCreate: (db,version){
+          db.execute('CREATE TABLE $_UserTableName($_UserID INTEGER PRIMARY KEY, $_UserName TEXT NOT NULL, $_UserHeight REAL)');
+      }
     );
   }
 }
